e9db6104c79f68d176867f6ae95f62d7724408b4
Jaye <wang.jaye@yahoo.com>
2017-08-21 00:06:40 -0700

    .gitignore is now working

---
+++ String.cpp
@@ -0,0 +1,9 @@
+//
+//  String.cpp
+//  dic
+//
+//  Created by Jaye Wang on 8/20/17.
+//  Copyright © 2017 JayeWang. All rights reserved.
+//
+
+#include "String.hpp"
---
+++ String.hpp
@@ -0,0 +1,14 @@
+//
+//  String.hpp
+//  dic
+//
+//  Created by Jaye Wang on 8/20/17.
+//  Copyright © 2017 JayeWang. All rights reserved.
+//
+
+#ifndef String_hpp
+#define String_hpp
+
+#include <stdio.h>
+
+#endif /* String_hpp */
